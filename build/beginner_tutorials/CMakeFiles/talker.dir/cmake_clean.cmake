1db5793731c7fd61829780e2e2d7f4b2d14bb799
zzjkf2009 <zzeng@terpmail.umd.edu>
2017-03-28 20:00:39 -0400

    modify executable name

---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/zejiang/ROS_beginner_tutorials_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/zejiang/ROS_beginner_tutorials_ws/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/zejiang/ROS_beginner_tutorials_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/zejiang/ROS_beginner_tutorials_ws/devel;/opt/ros/indigo".split(';') if "/home/zejiang/ROS_beginner_tutorials_ws/devel;/opt/ros/indigo" != "" else []
